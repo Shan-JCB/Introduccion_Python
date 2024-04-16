3387e7e88e40f306afe0058263b89e671f40e559
Shan-JCB <71997263@continental.edu.pe>
2024-04-15 22:03:53 -0500

    Se añadió el segundo ejercicio - from (Sebastian)

---
+++ Ejercicio_2/3.13 Calificaciones numéricas y literales..py
@@ -0,0 +1,9 @@
+calificacion = float(input("Ingrese la calificación: "))  #
+if calificacion > 9.0:
+    print("La calificación es A.")
+elif calificacion > 8.0:
+    print("La calificación es B.")
+elif calificacion >= 7.5:
+    print("La calificación es C.")
+else:
+    print("La calificación es R.")
